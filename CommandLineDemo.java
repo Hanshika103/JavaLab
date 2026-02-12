d3d469c5557d28ae4fe30f2b4363618e9956a137
Hanshika Mukati <hanshikamukati10@gmail.com>
2026-02-12 20:07:35 +0530

    Add files via upload
    Command Line in Java

---
+++ CommandLineDemo.java
@@ -0,0 +1,10 @@
+public class CommandLineDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Arguments are:");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
