f3b65b2dff02bf0ce68cbedbe51aa37713ec194d
Hanshika Mukati <hanshikamukati10@gmail.com>
2026-04-01 01:03:37 +0530

    Implement main method for launching Swing windows
    Added main method to launch different Swing frames.

--- SwingClass/MainFrame.java
+++ SwingClass/MainFrame.java
@@ -1,1 +1,17 @@
+import javax.swing.*;
 
+/*
+ * MAIN CLASS
+ * This class is used to launch different Swing windows.
+ * You can open any functionality from here.
+ */
+
+public class MainFrame {
+    public static void main(String[] args) {
+
+        // Open different frames (you can comment/uncomment)
+        new WordCountFrame();
+        // new PasswordFrame();
+        // new CheckboxFrame();
+    }
+}
